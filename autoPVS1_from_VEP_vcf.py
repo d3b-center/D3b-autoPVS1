d4ae38e32a885905a291bd24609c305376d33317
Miguel Brown <miguel.a.brown@gmail.com>
2023-08-25 18:57:39 +0000

    :hammer: heavy refactor with changes to pick rules

--- autoPVS1_from_VEP_vcf.py
+++ autoPVS1_from_VEP_vcf.py
@@ -5,7 +5,6 @@
 # Modified: 2022/10/05
 # Mod_author: Miguel Brown
 
-import re
 import sys
 import argparse
 from collections import namedtuple
@@ -16,40 +15,62 @@ from utils import get_transcript, vep_consequence_trans, VCFRecord
 __version__ = 'v0.3.0'
 
 
-lof_type = ['frameshift', 'nonsense', 'splice-5', 'splice-3', 'init-loss']
-vep_lof_list = ['frameshift', 'stop_gained', 'splice_donor', 'splice_acceptor', 'start_lost']
-VAR = namedtuple('VAR', ('varid', 'gene', 'trans', 'canonical', 'pick', 'record'))
+def pick_transcript(record, csq_fields, summary, csq_severity_order):
+    # Rules - is canonical, is in transcript reference
+    canon_idx = csq_fields.index("CANONICAL")
+    p_idx = csq_fields.index("PICK")
+    csq_idx = csq_fields.index("Consequence")
+    pick_value = "1"
+    canon_value = "YES"
+    r_idx = csq_fields.index('RefSeq')
+    picked_csqs = []
+    tx_candidates = []
 
-
-class AutoPVS1:
-    """Run AutoPVS1"""
-    @staticmethod
-    def get_vcfrecord(vcf):
-        if all(hasattr(vcf, attr) for attr in ['chrom', 'pos', 'ref', 'alt']):
-            return vcf
-        elif re.match(r'^((?:chr)?[\dXYMT]{1,2})-(\d+)-([ATCG]+)-([ATCG]+)$', vcf, re.I):
-            v = vcf.split("-")
-            v[0] = re.sub('chr', '', v[0], flags=re.I)
-            return VCFRecord(v[0], int(v[1]), v[2], v[3])
-        else:
-            raise TypeError("Wrong VCF Record")
-
-
-    def run_pvs1(self):
-        pvs1 = PVS1(self.vcfrecord, self.consequence, self.hgvs_c, 
-                    self.hgvs_p, self.transcript, self.genome_version)
-        return pvs1
+    for csq in record.info['CSQ']:
+        info_list = csq.split("|")
+        refseq_id = info_list[r_idx]
+        if info_list[p_idx] == pick_value:
+            pick = (get_transcript(refseq_id, transcripts_hg38), info_list)
+        if info_list[canon_idx] == canon_value:
+            if refseq_id != "":
+                # pdb.set_trace()
+                check = get_transcript(refseq_id, transcripts_hg38)
+                if check is not None:
+                    picked_csqs.append(info_list)
+                    tx_candidates.append(check)
+    # tie break with rank - if multiple share rank, go first best found
+    if len(picked_csqs) > 1:
+        best_rank = 1000
+        champ = 0
+        summary['rank'] += 1
+        for c in range(len(picked_csqs)):
+            # Some variants have "compound csqs" seprated by '&', subdivide and go with worst first
+            csq_values = picked_csqs[c][csq_idx].split("&")
+            for csq_value in csq_values:
+                score = csq_severity_order.index(csq_value)
+                if score < best_rank:
+                    # even if found, keep going in case a later rank is even worse
+                    best_rank = score
+                    champ = c
+        return tx_candidates[champ], picked_csqs[champ], summary
+    # go with PICK
+    if len(picked_csqs) == 0:
+        summary['pick'] +=1
+        return pick[0], pick[1], summary
+    else:
+        summary['canonical'] += 1
+        return tx_candidates[0], picked_csqs[0], summary
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--vep_vcf', help='VCF classify')
     parser.add_argument('--genome_version', help='Currently only hg38 or GRCh38 supported')
-    parser.add_argument('--tx_choice', help='Use VEP PICK or CANONICAL')
-
     parser.add_argument('--version', action='version',
                         version='%(prog)s {version}'.format(version=__version__))
     args = parser.parse_args()
+    lof_type = ['frameshift', 'nonsense', 'splice-5', 'splice-3', 'init-loss']
+    csq_severity_order = ["transcript_ablation","splice_acceptor_variant","splice_donor_variant","stop_gained","frameshift_variant","stop_lost","start_lost","transcript_amplification","feature_elongation","feature_truncation","inframe_insertion","inframe_deletion","missense_variant","protein_altering_variant","splice_donor_5th_base_variant","splice_region_variant","splice_donor_region_variant","splice_polypyrimidine_tract_variant","incomplete_terminal_codon_variant","start_retained_variant","stop_retained_variant","synonymous_variant","coding_sequence_variant","mature_miRNA_variant","5_prime_UTR_variant","3_prime_UTR_variant","non_coding_transcript_exon_variant","intron_variant","NMD_transcript_variant","non_coding_transcript_variant","coding_transcript_variant","upstream_gene_variant","downstream_gene_variant","TFBS_ablation","TFBS_amplification","TF_binding_site_variant","regulatory_region_ablation","regulatory_region_amplification","regulatory_region_variant","intergenic_variant","sequence_variant"]
 
     genome_version = args.genome_version
     in_vcf = pysam.VariantFile(args.vep_vcf, threads=8)
@@ -57,50 +78,14 @@ def main():
     # Use VEP PICK field to choose a representative transcript
     csq_fields = in_vcf.header.info['CSQ'].description.replace("Consequence annotations from Ensembl VEP. Format: ", "").split("|")
 
-    # get index of choice field
-    tx_pick = args.tx_choice
-    pick_value = '1'
-    if tx_pick != "PICK":
-        pick_value = "YES"
-    p_idx = csq_fields.index(tx_pick)
-
     print ("vcf_id",'SYMBOL','Feature','trans_name','consequence', 'strength_raw', 'strength','criterion', sep="\t")
+    summary = { "canonical": 0, "pick": 0, "rank": 0 }
     for record in in_vcf.fetch():
         # Parse CSQ for PICKed transcript
-        picked_csqs = []
-        picked_csq = 0
-        try:
-            for csq in record.info['CSQ']:
-                info_list = csq.split("|")
-                if info_list[p_idx] == pick_value:
-                    picked_csqs.append(csq)
-        except Exception as e:
-            sys.stderr.write(str(e) + '\n')
-            sys.stderr.write(record.contig + "\t" + str(record.pos) + '\n')
-            continue
-        if len(picked_csqs) > 1:
-            sys.stderr.write("WARN: More than 1 PICK detected, checking for preferred refseq\n")
-            r_ct = 0
-            r_idx = csq_fields.index('RefSeq')
-            for i in range(len(picked_csqs)):
-                check = picked_csqs[i].split('|')
-                if check[r_idx] != "":
-                    r_ct += 1
-                    picked_csq = i
-            # if refseq indeterminant, go with PICK
-            if r_ct != 1:
-                picked_csqs = []
-        if len(picked_csqs) == 0:
-            sys.stderr.write("WARN: No " + tx_pick + " detected or indeterminant, default to PICK\n")
-            p2_idx = csq_fields.index("PICK")
-            for csq in record.info['CSQ']:
-                info_list = csq.split("|")
-                if info_list[p2_idx] == '1':
-                    picked_csqs.append(csq)
-                    picked_csq = 0
         # Populate dict with CSQ key-value pairs
+        
+        transcript, first_picked, summary = pick_transcript(record, csq_fields, summary, csq_severity_order)
         info = {}
-        first_picked = picked_csqs[picked_csq].split('|')
         for i in range(len(first_picked)):
             info[csq_fields[i]] = first_picked[i]
             # adjust this filed to match behavior of original auto pvs1 script
@@ -108,12 +93,6 @@ def main():
                 info[csq_fields[i]] = first_picked[i].replace('%3D', '=')
 
         vcfrecord = VCFRecord(record.contig.replace('chr', ''), str(record.pos), record.ref, record.alts[0])
-        # Favor refseq ID
-        if info['RefSeq'] != '':
-            tx = info['RefSeq']
-        else:
-            tx = info['Feature']
-        transcript = get_transcript(tx, transcripts_hg38)
 
         consequence = vep_consequence_trans(info['Consequence'])
         vcf_id = "-".join([vcfrecord.chrom, str(vcfrecord.pos), vcfrecord.ref, vcfrecord.alt])
@@ -149,25 +128,7 @@ def main():
                   'Unmet',
                   'na',
                   sep="\t")
-
-        '''
-        if info['Function'] in ['splice-5', 'splice-3']:
-            pvs1 = PVS1(vcfrecord, info['Function'], info['pHGVS1'], info['MIMInheritance'], transcript)
-            splice = Splicing(vcfrecord, transcript)
-            print(vcf_id,
-                  pvs1.function,
-                  pvs1.strength_raw.name,
-                  pvs1.strength.name,
-                  splice.is_undergo_NMD,
-                  splice.has_cryptic_splice_site,
-                  splice.is_exon_skipping,
-                  splice.preserves_reading_frame,
-                  splice.is_critical_to_protein_func,
-                  splice.variant_removes_10_percent_of_protein,
-                  splice.is_critical_to_protein_func_detail,
-                  sep="\t")
-        '''
-
+    print("Summary of transcript pick categories: Canonical: {}, Rank: {}, Pick: {}".format(summary['canonical'], summary['rank'], summary['pick']), file=sys.stderr)
 
 if __name__ == '__main__':
     main()
